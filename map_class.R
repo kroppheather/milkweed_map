9a97754799d20295c8d48856cd8d22fb1503feea
Heather Kropp <kroppheather@gmail.com>
2022-08-01 11:12:47 -0400

    Update map_class.R

--- map_class.R
+++ map_class.R
@@ -26,7 +26,7 @@ minHeight <- cellStats(f06_29_2022[[6]], "min")
 height <- f06_29_2022[[6]]-minHeight
 
 treeM <- function(x){
-  ifelse(x > 11, 0, 1)
+  ifelse(x > 12, 0, 1)
 }
 treeMask <- calc(height, fun=treeM)
 
@@ -58,4 +58,49 @@ trainData <- known_pts[known_pts$type == "train",]
 
 trainMat <- extract(F06_29, trainData)
 trainDF <- data.frame(trainMat, st_drop_geometry(trainData))
+trainDF <- na.omit(trainDF)
 
+
+tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
+                   number = 10, # number 10 fold
+                   repeats = 10) # number of repeats
+###random forests
+#Typically square root of number of variables
+nbands <- 9 #20 bands of information
+rf.grid <- expand.grid(mtry=1:round(sqrt(nbands)))
+
+# set random seed for algorithm so you can get the same results when
+# running multiple times
+set.seed(43)
+
+#note that caret:: will make sure we use train from the caret package
+rf_model <- caret::train(x = trainDF[,1:9], #digital number data
+                         y = as.factor(trainDF[,13]), #land class we want to predict
+                         method = "rf", #use random forest
+                         metric="Accuracy", #assess by accuracy
+                         trainControl = tc, #use parameter tuning method
+                         tuneGrid = rf.grid) #parameter t
+
+
+#use the model to predict land cover class for the entire raster stack
+rf_prediction <- raster::predict(F06_29 , rf_model )
+# plot the land cover class (uses LCID number)
+
+plot(rf_prediction, breaks=c(.5,1.5,2.5), col= c("purple","grey"))
+
+
+validData <- known_pts[known_pts$type == "valid",]
+validVec <- extract(rf_prediction, validData)
+
+validDF <- data.frame(prediction=validVec, st_drop_geometry(trainData))
+
+
+rf_errorM = confusionMatrix(as.factor(validDF$prediction),as.factor(validDF$classID))
+
+milkRC <- function(x){ifelse(x == 1,1,0)}
+
+milkweed_06_29 <- calc(rf_prediction, milkRC)
+
+writeRaster(milkweed_06_29,
+            "E:/Google Drive/GIS/drone/campus/2022/out/classification/06_29/milkweed_rf_2.tif",
+            format="GTiff")
